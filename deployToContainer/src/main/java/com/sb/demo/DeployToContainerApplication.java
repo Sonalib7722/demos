b45c071b61e70ba55baee9fb4415ce5925b70db8
sonalib7722 <sonalib7722@gmail.com>
2023-02-16 16:13:43 +0530

    first commit

---
+++ deployToContainer/src/main/java/com/sb/demo/DeployToContainerApplication.java
@@ -0,0 +1,13 @@
+package com.sb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployToContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployToContainerApplication.class, args);
+	}
+
+}
