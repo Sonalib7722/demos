103a14069e76c358d51ff3da16f4c9a9d310773b
sonalib7722 <sonalib7722@gmail.com>
2023-02-16 16:50:06 +0530

    changes

--- deployToContainer/src/main/java/com/sb/demo/DeployToContainerApplication.java
+++ deployToContainer/src/main/java/com/sb/demo/DeployToContainerApplication.java
@@ -2,12 +2,18 @@ package com.sb.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DeployToContainerApplication {
+public class DeployToContainerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DeployToContainerApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DeployToContainerApplication.class);
+	}
 }
